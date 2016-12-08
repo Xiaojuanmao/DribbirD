edd67563ad351fd26ffce25cedda6c70b15b514d
Xiaojuanmao <daque@hustunique.com>
2016-12-08 23:58:57 +0800

    drawer

--- app/src/main/java/com/xjm/xxd/dribbird/ui/MainActivity.java
+++ app/src/main/java/com/xjm/xxd/dribbird/ui/MainActivity.java
@@ -2,12 +2,14 @@ package com.xjm.xxd.dribbird.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.view.MenuItem;
 
 import com.xjm.xxd.dribbird.R;
-import com.xjm.xxd.dribbird.account.TokenBean;
-import com.xjm.xxd.dribbird.account.TokenManager;
 import com.xjm.xxd.dribbird.presenter.activity.IMainActivityPresenter;
 import com.xjm.xxd.dribbird.ui.base.BaseActivity;
 import com.xjm.xxd.dribbird.view.MainActivityView;
@@ -20,8 +22,14 @@ import butterknife.ButterKnife;
 public class MainActivity extends BaseActivity implements
         MainActivityView {
 
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
     @BindView(R.id.tool_bar)
     Toolbar mToolbar;
+    @BindView(R.id.navigation_view)
+    NavigationView mNavigationView;
+
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Inject
     IMainActivityPresenter mPresenter;
@@ -36,15 +44,30 @@ public class MainActivity extends BaseActivity implements
 
         initViews();
 
-        TokenBean bean = TokenManager.getTokenBean();
-        if (bean != null) {
-            Log.e("here", "token ； " + bean.getAccessToken());
-        }
     }
 
     private void initViews() {
-
+        mToolbar.setTitle(R.string.main_activity_title);
         setSupportActionBar(mToolbar);
+
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setHomeButtonEnabled(true);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem menuItem) {
+                    menuItem.setChecked(true);
+                    mDrawerLayout.closeDrawers();
+                    return true;
+                }
+            });
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 
     }
 
