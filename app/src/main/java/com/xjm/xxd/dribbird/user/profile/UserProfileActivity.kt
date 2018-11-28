79b7e55bcb6d3b885a2c6a6b15b2620686295e58
da.que <da.que@ushow.media>
2018-11-28 10:59:42 +0800

    update gradle dependency

--- app/src/main/java/com/xjm/xxd/dribbird/user/profile/UserProfileActivity.kt
+++ app/src/main/java/com/xjm/xxd/dribbird/user/profile/UserProfileActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.CollapsingToolbarLayout
+import android.support.v7.widget.Toolbar
 import com.xjm.xxd.dribbird.R
 import com.xjm.xxd.dribbird.utils.StatusBarCompat
 import com.xjm.xxd.framework.kotlinext.bindView
@@ -12,13 +13,18 @@ import com.xjm.xxd.skeleton.mvp.MVPActivity
 class UserProfileActivity : MVPActivity<Presenter, Viewer>(),
         Viewer {
 
-    private val mCollaspingToolbar by bindView<CollapsingToolbarLayout>(R.id.collapsing_toolbar)
+    private val mCollapsingToolbar by bindView<CollapsingToolbarLayout>(R.id.collapsing_toolbar)
+    private val mToolbar by bindView<Toolbar>(R.id.tool_bar)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         StatusBarCompat.compat(this)
         setContentView(R.layout.activity_user_profile)
-        mCollaspingToolbar.title = getString(R.string.app_name)
+
+        mToolbar.title = getString(R.string.app_name)
+        mToolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 
     override fun createPresenter(): Presenter = UserProfilePresenter()
